63080b6cfa63144aa5dc66687408e81aed2e48ed
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2020-07-01 07:07:34 +0530

    Fix store_id bug in item recommendation

--- recommendations/fetch_data.py
+++ recommendations/fetch_data.py
@@ -6,18 +6,21 @@ from .rec import * #TODO: fix this with correct path?
 #################################
 # Item recommendation API logic #
 #################################
-def recommendItemsInStore(user_id, k):
+def recommendItemsInStore(user_id, store_id, k):
   user_to_purchase_counts={}
   for user in CustomUser.objects.filter():
     store_item_orders={}
     orders=Order.objects.filter(user=user)
     for order in orders:
-      products=OrderItems.objects.filter(order=order)
-      for product in products:
-        product_id=product.item.item_id
-        if product_id not in store_item_orders:
-          store_item_orders[product_id]=0
-        store_item_orders[product_id]+=1
+      store=order.store
+      order_store_id=store.store_id
+      if order_store_id==store_id:
+        products=OrderItems.objects.filter(order=order)
+        for product in products:
+          product_id=product.item.item_id
+          if product_id not in store_item_orders:
+            store_item_orders[product_id]=0
+          store_item_orders[product_id]+=1
     user_to_purchase_counts[user.id]=store_item_orders
   data_list=[]
   for user in user_to_purchase_counts.keys():
